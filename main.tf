5f5598f6e9c0cc48d38a8d53830f14da46dde234
Rui Lopes <rgl@ruilopes.com>
2024-01-14 12:38:35 +0000

    upgrade dependencies

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
     # see https://github.com/hashicorp/terraform-provider-aws
     aws = {
       source  = "hashicorp/aws"
-      version = "5.31.0"
+      version = "5.32.1"
     }
   }
 }
